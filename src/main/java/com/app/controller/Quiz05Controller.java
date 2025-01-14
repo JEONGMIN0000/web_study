d76ca7e19dd1ce1c630645cf1d0be40de23116f7
JEONGMIN0000 <leewjdals122900@gmail.com>
2025-01-14 10:30:31 +0900

    요청 & 데이터 전달 연습

---
+++ src/main/java/com/app/controller/Quiz05Controller.java
@@ -0,0 +1,115 @@
+package com.app.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.app.dto.ResponseItem;
+
+@Controller
+public class Quiz05Controller {
+	
+	//1
+	
+	//1-1
+	@RequestMapping("/request1-1")
+	public String request1_1(HttpServletRequest request) {
+		
+		System.out.println(request.getParameter("category"));
+		System.out.println(request.getParameter("product"));
+		
+		return "/quiz/quiz05/quiz05";
+	}
+	
+	//1-2
+	@RequestMapping("/request1-2")
+	public String request1_2(@RequestParam String category, @RequestParam String product) {
+		
+		System.out.println(category);
+		System.out.println(product);
+		
+		return "/quiz/quiz05/quiz05";
+	}
+	//1-3
+	@RequestMapping("/request1-3")
+	public String request1_3(@ModelAttribute ResponseItem item) {
+		
+		System.out.println(item.getCategory());
+		System.out.println(item.getProduct());
+		
+		return "/quiz/quiz05/quiz05";
+	}
+	
+	//1-4
+		@RequestMapping("/request1-4")
+		public String request1_4(@RequestParam Map<String,String> paramMap) {
+			
+			System.out.println(paramMap.get("category"));
+			System.out.println(paramMap.get("product"));
+			
+			return "/quiz/quiz05/quiz05";
+		}
+	
+	//2
+	
+	//2-1
+	@RequestMapping("/response1-1")
+	public String response1_1(HttpServletRequest request) {
+		
+		request.setAttribute("response001", "ㅎㅇ");
+		request.setAttribute("response099", "ㅂㅇ");
+		
+		return "/quiz/quiz05/view";
+	}
+	
+	//2-2
+	@RequestMapping("/response1-2")
+	public String response1_2(Model model) {
+		
+		model.addAttribute("response001", "ㅎㅇ");
+		model.addAttribute("response099", "ㅂㅇ");
+		
+		return "/quiz/quiz05/view";
+	}
+	//2-3
+	@RequestMapping("/response1-3")
+	public ModelAndView response1_3(ModelAndView mav) {
+		
+		mav.setViewName("/quiz/quiz05/view");
+		mav.addObject("response001", "ㅎㅇ");
+		mav.addObject("response099", "ㅂㅇ");
+		
+		return mav;
+	}
+	
+	//2-4
+	@RequestMapping("/response1-4")
+//	public String response1_4(ResponseItem item) { //@ModelAttribute 어노테이션 생략가능
+	public String response1_4(@ModelAttribute ResponseItem item) {
+		
+		item.setResponse001("ㅎㅇ");
+		item.setResponse099("ㅂㅇ");
+		
+		return "/quiz/quiz05/view";
+	}
+	
+	//2-4
+	@RequestMapping("/response1-4")
+	public String response1_4(Model model) {
+		
+		ResponseItem item = new ResponseItem();
+		item.setResponse001("ㅎㅇ");
+		item.setResponse099("ㅂㅇ");
+		
+		model.addAttribute("responseItem", item);
+		
+		return "/quiz/quiz05/view";
+	}
+}
---
+++ src/main/java/com/app/dto/ResponseItem.java
@@ -0,0 +1,38 @@
+package com.app.dto;
+
+public class ResponseItem {
+	
+	String category;
+	String product;
+	
+	String response001;
+	String response099;
+	
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public String getResponse001() {
+		return response001;
+	}
+	public void setResponse001(String response001) {
+		this.response001 = response001;
+	}
+	public String getResponse099() {
+		return response099;
+	}
+	public void setResponse099(String response099) {
+		this.response099 = response099;
+	}
+	
+	
+	
+}
