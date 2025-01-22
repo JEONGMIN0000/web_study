8b47e3490dfdf3b7d1afa8bc09310a79a456de41
JEONGMIN0000 <leewjdals122900@gmail.com>
2025-01-22 13:51:39 +0900

    공통코드 상수화 인터페이스에 정리

---
+++ src/main/java/com/app/common/CommonCode.java
@@ -0,0 +1,14 @@
+package com.app.common;
+
+public interface CommonCode {
+	
+	//서비스내에서 사용하는 모든 상수형 코드들을 모아놓은 파일
+	public static final String USERTYPE_CUSTOMER = "CUS";
+	public static final String USERTYPE_ADMIN = "ADM";
+	
+	//Room
+	public String ROOM_VIEWTYPE_OCEAN = "OCN";
+	public String ROOM_VIEWTYPE_CITY = "CTY";
+	public String ROOM_VIEWTYPE_MOUNTAIN = "MOT";
+	
+}
