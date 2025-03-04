a3aec706ee7e9a0456f02d7a0a92b897caa9c119
JEONGMIN0000 <leewjdals122900@gmail.com>
2025-03-04 14:27:43 +0900

    사용자 비밀번호 암호화처리

--- src/main/java/com/app/controller/customer/CustomerController.java
+++ src/main/java/com/app/controller/customer/CustomerController.java
@@ -2,6 +2,7 @@ package com.app.controller.customer;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -39,6 +40,7 @@ import com.app.service.file.FileService;
 import com.app.service.user.UserService;
 import com.app.util.FileManager;
 import com.app.util.LoginManager;
+import com.app.util.SHA256Encryptor;
 import com.app.validator.UserCustomValidator;
 
 import lombok.extern.slf4j.Slf4j;
@@ -101,6 +103,16 @@ public class CustomerController {
 		
 		
 		user.setUserType( CommonCode.USERTYPE_CUSTOMER );
+		String encPw;
+		try {
+			//사용자 비밀번호 암호화 처리
+			encPw = SHA256Encryptor.encrypt(user.getPw());
+			user.setPw(encPw);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			//잘못됐다 안내 ->  페이지 전환
+		}
+		
 		int result = userService.saveUser(user); //DB 에 user정보 저장 (가입)
 		
 		System.out.println("회원가입 처리 결과 : " + result);
@@ -192,6 +204,12 @@ public class CustomerController {
 		
 		//user 로그인 할 수 있게 정보가 들어있는지! 확인!
 		user.setUserType( CommonCode.USERTYPE_CUSTOMER );
+		
+		try {
+			user.setPw(SHA256Encryptor.encrypt(user.getPw()));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 		User loginUser = userService.checkUserLogin(user);
 		
 		if(loginUser == null) { // 아이디X? 아이디O&비번X  null
